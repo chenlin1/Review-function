0953032fbc463accfea2417473b0f53f0cfeebc4
MacBook <macbook@MacBookdeMacBook-Pro.local>
2019-03-19 17:11:10 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentApp from './CommentApp';
-ReactDOM.render(<CommentApp />, document.getElementById('root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import CommentApp from './CommentApp';
+// // import CommentApp from './ContextIndex'
+// ReactDOM.render(<CommentApp />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import CommentApp from './containers/CommentApp'
+import commentsReducer from './reducers/comments'
+import './App.css'
 
+const store = createStore(commentsReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <CommentApp />
+  </Provider>,
+  document.getElementById('root')
+);
